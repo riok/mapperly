a94c5bd4c0adda86f22e74ad9b44bf1c0d16592a
latonz <lars@riok.ch>
2022-02-17 14:31:47 +0100

    chore: add test to upgrade user nullability

---
+++ test/Riok.Mapperly.Tests/_snapshots/NullableTest.ShouldUpgradeNullabilityInDisabledNullableContext.verified.cs
@@ -0,0 +1,13 @@
+﻿//HintName: Mapper.g.cs
+#nullable enable
+public sealed class Mapper : IMapper
+{
+    public static readonly IMapper Instance = new Mapper();
+    public B? Map(A? source)
+    {
+        if (source == null)
+            return default;
+        var target = new B();
+        return target;
+    }
+}
