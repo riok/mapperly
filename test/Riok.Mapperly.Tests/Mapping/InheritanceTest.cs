75f6e759ee24721a520dba55ce911ef4ab6eb340
Manuel Allenspach <manuel@riok.ch>
2022-08-29 09:29:40 +0200

    fix: for interfaces, include properties from inherited interfaces in mappings

---
+++ test/Riok.Mapperly.Tests/Mapping/InheritanceTest.cs
@@ -0,0 +1,159 @@
+namespace Riok.Mapperly.Tests.Mapping;
+
+public class InheritanceTest
+{
+    [Fact]
+    public void SimpleClassInheritanceInSource()
+    {
+        var source = TestSourceBuilder.Mapping(
+            "B",
+            "C",
+            "class A { public string StringValue1 { get; set; } }",
+            "class B : A { public string StringValue2 { get; set; } }",
+            "class C { public string StringValue1 { get; set; } public string StringValue2 { get; set; } }");
+
+        TestHelper.GenerateSingleMapperMethodBody(source)
+            .Should()
+            .Be(@"var target = new C();
+    target.StringValue1 = source.StringValue1;
+    target.StringValue2 = source.StringValue2;
+    return target;".ReplaceLineEndings());
+    }
+
+    [Fact]
+    public void MultipleClassInheritanceInSource()
+    {
+        var source = TestSourceBuilder.Mapping(
+            "D",
+            "E",
+            "class A { public string StringValue1 { get; set; } }",
+            "class B : A { public string StringValue2 { get; set; } }",
+            "class C : B { public string StringValue3 { get; set; } }",
+            "class D : C { public string StringValue4 { get; set; } }",
+            "class E { public string StringValue1 { get; set; } public string StringValue2 { get; set; } public string StringValue3 { get; set; } public string StringValue4 { get; set; } }");
+
+        TestHelper.GenerateSingleMapperMethodBody(source)
+            .Should()
+            .Be(@"var target = new E();
+    target.StringValue1 = source.StringValue1;
+    target.StringValue2 = source.StringValue2;
+    target.StringValue3 = source.StringValue3;
+    target.StringValue4 = source.StringValue4;
+    return target;".ReplaceLineEndings());
+    }
+
+    [Fact]
+    public void SimpleClassInheritanceInTarget()
+    {
+        var source = TestSourceBuilder.Mapping(
+            "A",
+            "C",
+            "class A { public string StringValue1 { get; set; } public string StringValue2 { get; set; } }",
+            "class B { public string StringValue1 { get; set; } }",
+            "class C : B { public string StringValue2 { get; set; } }");
+
+        TestHelper.GenerateSingleMapperMethodBody(source)
+            .Should()
+            .Be(@"var target = new C();
+    target.StringValue2 = source.StringValue2;
+    target.StringValue1 = source.StringValue1;
+    return target;".ReplaceLineEndings());
+    }
+
+    [Fact]
+    public void MultipleClassInheritanceInTarget()
+    {
+        var source = TestSourceBuilder.Mapping(
+            "A",
+            "E",
+            "class A { public string StringValue1 { get; set; } public string StringValue2 { get; set; } public string StringValue3 { get; set; } public string StringValue4 { get; set; } }",
+            "class B { public string StringValue1 { get; set; } }",
+            "class C : B { public string StringValue2 { get; set; } }",
+            "class D : C { public string StringValue3 { get; set; } }",
+            "class E : D { public string StringValue4 { get; set; } }");
+
+        TestHelper.GenerateSingleMapperMethodBody(source)
+            .Should()
+            .Be(@"var target = new E();
+    target.StringValue4 = source.StringValue4;
+    target.StringValue3 = source.StringValue3;
+    target.StringValue2 = source.StringValue2;
+    target.StringValue1 = source.StringValue1;
+    return target;".ReplaceLineEndings());
+    }
+
+    [Fact]
+    public void ClassInheritanceInSourceAndTarget()
+    {
+        var source = TestSourceBuilder.Mapping(
+            "B",
+            "D",
+            "class A { public string StringValue1 { get; set; } }",
+            "class B : A { public string StringValue2 { get; set; } }",
+            "class C { public string StringValue1 { get; set; } }",
+            "class D : C { public string StringValue2 { get; set; } }");
+
+        TestHelper.GenerateSingleMapperMethodBody(source)
+            .Should()
+            .Be(@"var target = new D();
+    target.StringValue2 = source.StringValue2;
+    target.StringValue1 = source.StringValue1;
+    return target;".ReplaceLineEndings());
+    }
+
+    [Fact]
+    public void SimpleInterfaceInheritanceInSource()
+    {
+        var source = TestSourceBuilder.Mapping(
+            "B",
+            "C",
+            "interface A { string StringValue1 { get; set; } }",
+            "interface B : A { string StringValue2 { get; set; } }",
+            "class C { public string StringValue1 { get; set; } public string StringValue2 { get; set; } }");
+
+        TestHelper.GenerateSingleMapperMethodBody(source)
+            .Should()
+            .Be(@"var target = new C();
+    target.StringValue1 = source.StringValue1;
+    target.StringValue2 = source.StringValue2;
+    return target;".ReplaceLineEndings());
+    }
+
+    [Fact]
+    public void MultipleInterfaceInheritanceInSource()
+    {
+        var source = TestSourceBuilder.Mapping(
+            "D",
+            "E",
+            "interface A { string StringValue1 { get; set; } }",
+            "interface B : A { string StringValue2 { get; set; } }",
+            "interface C : B { string StringValue3 { get; set; } }",
+            "interface D : C { string StringValue4 { get; set; } }",
+            "class E { public string StringValue1 { get; set; } public string StringValue2 { get; set; } public string StringValue3 { get; set; } public string StringValue4 { get; set; } }");
+
+        TestHelper.GenerateSingleMapperMethodBody(source)
+            .Should()
+            .Be(@"var target = new E();
+    target.StringValue1 = source.StringValue1;
+    target.StringValue2 = source.StringValue2;
+    target.StringValue3 = source.StringValue3;
+    target.StringValue4 = source.StringValue4;
+    return target;".ReplaceLineEndings());
+    }
+
+    [Fact]
+    public void InterfaceInheritanceInSourceAndTarget()
+    {
+        var source = TestSourceBuilder.MapperWithBodyAndTypes(
+            "partial void ToD(B source, D target);",
+            "interface A { string StringValue1 { get; set; } }",
+            "interface B : A { string StringValue2 { get; set; } }",
+            "interface C : B { string StringValue1 { get; set; } }",
+            "interface D : C { string StringValue2 { get; set; } }");
+
+        TestHelper.GenerateSingleMapperMethodBody(source)
+            .Should()
+            .Be(@"target.StringValue2 = source.StringValue2;
+    target.StringValue1 = source.StringValue1;");
+    }
+}
