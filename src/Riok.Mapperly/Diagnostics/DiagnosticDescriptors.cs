6aa7be97af79da6b356f1a7d9b35385ae5a0452f
latonz <lars@riok.ch>
2022-09-20 17:06:49 +0200

    chore: remove target type instantiation of diagnostic descriptors

--- src/Riok.Mapperly/Diagnostics/DiagnosticDescriptors.cs
+++ src/Riok.Mapperly/Diagnostics/DiagnosticDescriptors.cs
@@ -4,9 +4,10 @@ namespace Riok.Mapperly.Diagnostics;
 
 // if adjusted, these files may need adjustments as well:
 // analyzer-diagnostics.md, AnalyzerReleases.Shipped.md and AnalyzerReleases.Unshipped.md
+// cannot use target-typed new: https://github.com/dotnet/roslyn-analyzers/issues/5828
 internal static class DiagnosticDescriptors
 {
-    public static readonly DiagnosticDescriptor UnsupportedMappingMethodSignature = new(
+    public static readonly DiagnosticDescriptor UnsupportedMappingMethodSignature = new DiagnosticDescriptor(
         "RMG001",
         "Has an unsupported mapping method signature",
         "{0} has an unsupported mapping method signature",
@@ -14,7 +15,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Error,
         true);
 
-    public static readonly DiagnosticDescriptor NoParameterlessConstructorFound = new(
+    public static readonly DiagnosticDescriptor NoParameterlessConstructorFound = new DiagnosticDescriptor(
         "RMG002",
         "No accessible parameterless constructor found",
         "{0} has no accessible parameterless constructor",
@@ -22,7 +23,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Error,
         true);
 
-    public static readonly DiagnosticDescriptor EnumNameMappingNoOverlappingValuesFound = new(
+    public static readonly DiagnosticDescriptor EnumNameMappingNoOverlappingValuesFound = new DiagnosticDescriptor(
         "RMG003",
         "No overlapping enum members found",
         "{0} and {1} don't have overlapping enum member names, mapping will therefore always result in an exception",
@@ -30,7 +31,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Warning,
         true);
 
-    public static readonly DiagnosticDescriptor IgnoredTargetPropertyNotFound = new(
+    public static readonly DiagnosticDescriptor IgnoredTargetPropertyNotFound = new DiagnosticDescriptor(
         "RMG004",
         "Ignored target property not found",
         "Ignored target property {0} on {1} was not found",
@@ -38,7 +39,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Warning,
         true);
 
-    public static readonly DiagnosticDescriptor ConfiguredMappingTargetPropertyNotFound = new(
+    public static readonly DiagnosticDescriptor ConfiguredMappingTargetPropertyNotFound = new DiagnosticDescriptor(
         "RMG005",
         "Mapping target property not found",
         "Specified property {0} on mapping target type {1} was not found",
@@ -46,7 +47,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Error,
         true);
 
-    public static readonly DiagnosticDescriptor ConfiguredMappingSourcePropertyNotFound = new(
+    public static readonly DiagnosticDescriptor ConfiguredMappingSourcePropertyNotFound = new DiagnosticDescriptor(
         "RMG006",
         "Mapping source property not found",
         "Specified property {0} on source type {1} was not found",
@@ -54,7 +55,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Error,
         true);
 
-    public static readonly DiagnosticDescriptor CouldNotMapProperty = new(
+    public static readonly DiagnosticDescriptor CouldNotMapProperty = new DiagnosticDescriptor(
         "RMG007",
         "Could not map property",
         "Could not map property {0}.{1} of type {2} to {3}.{4} of type {5}",
@@ -62,7 +63,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Error,
         true);
 
-    public static readonly DiagnosticDescriptor CouldNotCreateMapping = new(
+    public static readonly DiagnosticDescriptor CouldNotCreateMapping = new DiagnosticDescriptor(
         "RMG008",
         "Could not create mapping",
         "Could not create mapping from {0} to {1}. Consider implementing the mapping manually.",
@@ -70,7 +71,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Error,
         true);
 
-    public static readonly DiagnosticDescriptor CannotMapToReadOnlyProperty = new(
+    public static readonly DiagnosticDescriptor CannotMapToReadOnlyProperty = new DiagnosticDescriptor(
         "RMG009",
         "Cannot map to read only property",
         "Cannot map property {0}.{1} of type {2} to read only property {3}.{4} of type {5}",
@@ -78,7 +79,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Info,
         true);
 
-    public static readonly DiagnosticDescriptor CannotMapFromWriteOnlyProperty = new(
+    public static readonly DiagnosticDescriptor CannotMapFromWriteOnlyProperty = new DiagnosticDescriptor(
         "RMG010",
         "Cannot map from write only property",
         "Cannot map from write only property {0}.{1} of type {2} to property {3}.{4} of type {5}",
@@ -86,7 +87,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Info,
         true);
 
-    public static readonly DiagnosticDescriptor CannotMapToWriteOnlyPropertyPath = new(
+    public static readonly DiagnosticDescriptor CannotMapToWriteOnlyPropertyPath = new DiagnosticDescriptor(
         "RMG011",
         "Cannot map to write only property path",
         "Cannot map from property {0}.{1} of type {2} to write only property path {3}.{4} of type {5}",
@@ -94,7 +95,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Info,
         true);
 
-    public static readonly DiagnosticDescriptor MappingSourcePropertyNotFound = new(
+    public static readonly DiagnosticDescriptor MappingSourcePropertyNotFound = new DiagnosticDescriptor(
         "RMG012",
         "Mapping source property not found",
         "Property {0} on source type {1} was not found",
@@ -102,7 +103,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Info,
         true);
 
-    public static readonly DiagnosticDescriptor NoConstructorFound = new(
+    public static readonly DiagnosticDescriptor NoConstructorFound = new DiagnosticDescriptor(
         "RMG013",
         "No accessible constructor with mappable arguments found",
         "{0} has no accessible constructor with mappable arguments",
@@ -110,7 +111,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Error,
         true);
 
-    public static readonly DiagnosticDescriptor CannotMapToConfiguredConstructor = new(
+    public static readonly DiagnosticDescriptor CannotMapToConfiguredConstructor = new DiagnosticDescriptor(
         "RMG014",
         "Cannot map to the configured constructor to be used by Mapperly",
         "Cannot map from {0} to the configured constructor {1}",
@@ -118,7 +119,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Warning,
         true);
 
-    public static readonly DiagnosticDescriptor CannotMapToInitOnlyPropertyPath = new(
+    public static readonly DiagnosticDescriptor CannotMapToInitOnlyPropertyPath = new DiagnosticDescriptor(
         "RMG015",
         "Cannot map to init only property path",
         "Cannot map from property {0}.{1} of type {2} to init only property path {3}.{4} of type {5}",
@@ -126,7 +127,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Info,
         true);
 
-    public static readonly DiagnosticDescriptor InitOnlyPropertyDoesNotSupportPaths = new(
+    public static readonly DiagnosticDescriptor InitOnlyPropertyDoesNotSupportPaths = new DiagnosticDescriptor(
         "RMG016",
         "Init only property cannot handle target paths",
         "Cannot map to init only property path {0}.{1}",
@@ -134,7 +135,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Error,
         true);
 
-    public static readonly DiagnosticDescriptor MultipleConfigurationsForInitOnlyProperty = new(
+    public static readonly DiagnosticDescriptor MultipleConfigurationsForInitOnlyProperty = new DiagnosticDescriptor(
         "RMG017",
         "An init only property can have one configuration at max",
         "The init only property {0}.{1} can have one configuration at max",
@@ -142,7 +143,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Warning,
         true);
 
-    public static readonly DiagnosticDescriptor PartialStaticMethodInInstanceMapper = new(
+    public static readonly DiagnosticDescriptor PartialStaticMethodInInstanceMapper = new DiagnosticDescriptor(
         "RMG018",
         "Partial static mapping method in an instance mapper",
         "{0} is a partial static mapping method in an instance mapper. Static mapping methods are only supported in static mappers.",
@@ -150,7 +151,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Error,
         true);
 
-    public static readonly DiagnosticDescriptor PartialInstanceMethodInStaticMapper = new(
+    public static readonly DiagnosticDescriptor PartialInstanceMethodInStaticMapper = new DiagnosticDescriptor(
         "RMG019",
         "Partial instance mapping method in a static mapper",
         "{0} is a partial instance mapping method in a static mapper. Instance mapping methods are only supported in instance (non-static) mappers.",
@@ -158,7 +159,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Error,
         true);
 
-    public static readonly DiagnosticDescriptor SourcePropertyNotMapped = new(
+    public static readonly DiagnosticDescriptor SourcePropertyNotMapped = new DiagnosticDescriptor(
         "RMG020",
         "Source property is not mapped to any target property",
         "The property {0} on the mapping source type {1} is not mapped to any property on the mapping target type {2}",
@@ -166,7 +167,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Info,
         true);
 
-    public static readonly DiagnosticDescriptor IgnoredSourcePropertyNotFound = new(
+    public static readonly DiagnosticDescriptor IgnoredSourcePropertyNotFound = new DiagnosticDescriptor(
         "RMG021",
         "Ignored source property not found",
         "Ignored source property {0} on {1} was not found",
@@ -174,7 +175,7 @@ internal static class DiagnosticDescriptors
         DiagnosticSeverity.Warning,
         true);
 
-    public static readonly DiagnosticDescriptor InvalidObjectFactorySignature = new(
+    public static readonly DiagnosticDescriptor InvalidObjectFactorySignature = new DiagnosticDescriptor(
         "RMG022",
         "Invalid object factory signature",
         "The object factory {0} has an invalid signature",
