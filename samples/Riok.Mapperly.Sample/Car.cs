82c784487e0a3a69c265c2068a3b8e2725384e0c
Manuel <manuel@riok.ch>
2023-01-12 10:31:28 +0100

    docs: add sample (#223)

---
+++ samples/Riok.Mapperly.Sample/Car.cs
@@ -0,0 +1,39 @@
+namespace Riok.Mapperly.Sample;
+
+public class Car
+{
+    public string Name { get; set; } = string.Empty;
+
+    public int NumberOfSeats { get; set; }
+
+    public CarColor Color { get; set; }
+
+    public Manufacturer? Manufacturer { get; set; }
+
+    public List<Tire> Tires { get; } = new List<Tire>();
+}
+
+public enum CarColor
+{
+    Black = 1,
+    Blue = 2,
+    White = 3,
+}
+
+public class Manufacturer
+{
+    public Manufacturer(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public int Id { get; }
+
+    public string Name { get; }
+}
+
+public class Tire
+{
+    public string Description { get; set; } = string.Empty;
+}
