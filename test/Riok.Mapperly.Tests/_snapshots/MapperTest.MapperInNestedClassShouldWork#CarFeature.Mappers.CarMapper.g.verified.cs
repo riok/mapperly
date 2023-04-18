80a193ac8ca6e073c3b320d8b148a58c8ee6c68f
Lars <lars@riok.ch>
2023-04-18 08:46:06 +0200

    feat: support mappers nested in classes (#354)

---
+++ test/Riok.Mapperly.Tests/_snapshots/MapperTest.MapperInNestedClassShouldWork#CarFeature.Mappers.CarMapper.g.verified.cs
@@ -0,0 +1,15 @@
+﻿//HintName: CarFeature.Mappers.CarMapper.g.cs
+#nullable enable
+public static partial class CarFeature
+{
+    public static partial class Mappers
+    {
+        public partial class CarMapper
+        {
+            public partial int ToInt(double value)
+            {
+                return (int)value;
+            }
+        }
+    }
+}
