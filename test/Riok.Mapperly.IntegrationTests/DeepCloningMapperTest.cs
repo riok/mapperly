b9a1f1911637526c4ff3afa3ddc0841b6b417041
Tim M <49349513+TimothyMakkison@users.noreply.github.com>
2023-04-30 16:43:03 +0100

    fix: update IsImmutable to identify valid types on framework (#388)

---
+++ test/Riok.Mapperly.IntegrationTests/DeepCloningMapperTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Riok.Mapperly.IntegrationTests.Mapper;
+using Riok.Mapperly.IntegrationTests.Models;
+using VerifyXunit;
+using Xunit;
+
+namespace Riok.Mapperly.IntegrationTests
+{
+    [UsesVerify]
+    public class DeepCloningMapperTest : BaseMapperTest
+    {
+        [Fact]
+        public Task SnapshotGeneratedSource()
+        {
+            var path = GetGeneratedMapperFilePath(nameof(DeepCloningMapper));
+            return Verifier.VerifyFile(path);
+        }
+
+        [Fact]
+        public Task RunMappingShouldWork()
+        {
+            var model = NewTestObj();
+            var dto = DeepCloningMapper.Copy(model);
+            return Verifier.Verify(dto);
+        }
+
+        [Fact]
+        public void RunIdMappingShouldWork()
+        {
+            var source = new IdObject { IdValue = 20 };
+            var copy = DeepCloningMapper.Copy(source);
+            source.Should().NotBeSameAs(copy);
+            copy.IdValue.Should().Be(20);
+        }
+    }
+}
