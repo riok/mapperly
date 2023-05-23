125742e799fb4ed166750ddc955750f5ab50f0cd
Tim M <49349513+TimothyMakkison@users.noreply.github.com>
2023-05-23 07:24:04 +0100

    fix: prevent ctormapping using inaccessible constructors (#456)

--- src/Riok.Mapperly/Descriptors/MappingBuilders/CtorMappingBuilder.cs
+++ src/Riok.Mapperly/Descriptors/MappingBuilders/CtorMappingBuilder.cs
@@ -16,12 +16,14 @@ public static class CtorMappingBuilder
             return null;
 
         // resolve ctors which have the source as single argument
-        var ctorMethod = namedTarget.InstanceConstructors.FirstOrDefault(
-            m =>
-                m.Parameters.Length == 1
-                && SymbolEqualityComparer.Default.Equals(m.Parameters[0].Type, ctx.Source)
-                && ctx.Source.HasSameOrStricterNullability(m.Parameters[0].Type)
-        );
+        var ctorMethod = namedTarget.InstanceConstructors
+            .Where(x => x.IsAccessible())
+            .FirstOrDefault(
+                m =>
+                    m.Parameters.Length == 1
+                    && SymbolEqualityComparer.Default.Equals(m.Parameters[0].Type, ctx.Source)
+                    && ctx.Source.HasSameOrStricterNullability(m.Parameters[0].Type)
+            );
 
         return ctorMethod == null ? null : new CtorMapping(ctx.Source, ctx.Target);
     }
