b6836b277e23a0028f6a762880be2c9efe4bff20
Tim M <49349513+TimothyMakkison@users.noreply.github.com>
2023-06-02 14:00:17 +0100

    feat: added source generator benchmark (#419)
    Co-authored-by: Odai Wattad <45322451+odaiwa@users.noreply.github.co

---
+++ benchmarks/Riok.Mapperly.Benchmarks/MappingBenchmarks.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+using Riok.Mapperly.IntegrationTests;
+using Riok.Mapperly.IntegrationTests.Dto;
+using Riok.Mapperly.IntegrationTests.Mapper;
+using Riok.Mapperly.IntegrationTests.Models;
+
+namespace Riok.Mapperly.Benchmarks;
+
+[MemoryDiagnoser]
+[InProcess]
+[HtmlExporter]
+[JsonExporterAttribute.Full]
+public class MappingBenchmarks
+{
+    private readonly TestObject _testObject;
+    private readonly TestObjectDto _testObjectDto;
+    private readonly IdObject _idObject;
+
+    public MappingBenchmarks()
+    {
+        _testObject = BaseMapperTest.NewTestObj();
+        _testObjectDto = StaticTestMapper.MapToDto(_testObject);
+        _idObject = new IdObject { IdValue = 143 };
+    }
+
+    [Benchmark(Description = "MapComplexToDto")]
+    public TestObjectDto MapComplexToDto() => StaticTestMapper.MapToDto(_testObject);
+
+    [Benchmark(Description = "MapComplexToExistingDto")]
+    public void MapComplexToExistingDto() => StaticTestMapper.UpdateDto(_testObject, _testObjectDto);
+
+    [Benchmark(Description = "MapSimpleDeepClone")]
+    public IdObject MapSimpleDeepClone() => DeepCloningMapper.Copy(_idObject);
+
+    [Benchmark(Description = "MapComplexDeepClone")]
+    public TestObject MapComplexDeepClone() => DeepCloningMapper.Copy(_testObject);
+}
