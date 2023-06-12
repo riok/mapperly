e51f961de40a7ff3a7b3dbc936074fd7fbdb811b
Timothy Makkison <timmakkison@gmail.com>
2023-06-12 21:11:12 +0100

    feat: refactored `UniqueNameBuilder`

--- src/Riok.Mapperly/Helpers/UniqueNameBuilder.cs
+++ src/Riok.Mapperly/Helpers/UniqueNameBuilder.cs
@@ -3,31 +3,46 @@ namespace Riok.Mapperly.Helpers;
 public class UniqueNameBuilder
 {
     private readonly HashSet<string> _usedNames;
+    private readonly UniqueNameBuilder? _parentScope;
 
     public UniqueNameBuilder()
     {
         _usedNames = new HashSet<string>();
     }
 
-    private UniqueNameBuilder(IEnumerable<string> usedNames)
+    private UniqueNameBuilder(UniqueNameBuilder parentScope)
     {
-        _usedNames = new HashSet<string>(usedNames);
+        _usedNames = new HashSet<string>();
+        _parentScope = parentScope;
     }
 
     public void Reserve(string name) => _usedNames.Add(name);
 
-    public UniqueNameBuilder NewScope() => new(_usedNames);
+    public UniqueNameBuilder NewScope() => new(this);
 
     public string New(string name)
     {
         var i = 0;
         var uniqueName = name;
-        while (!_usedNames.Add(uniqueName))
+        while (Contains(uniqueName))
         {
             i++;
             uniqueName = name + i;
         }
 
+        _usedNames.Add(uniqueName);
+
         return uniqueName;
+    }
+
+    private bool Contains(string name)
+    {
+        if (_usedNames.Contains(name))
+            return true;
+
+        if (_parentScope != null)
+            return _parentScope.Contains(name);
+
+        return false;
     }
 }
