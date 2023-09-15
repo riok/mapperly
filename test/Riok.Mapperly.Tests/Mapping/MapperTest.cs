cfbf9d208e62ae168c333333174c63eaad5d732c
Timothy Makkison <timmakkison@gmail.com>
2023-09-15 12:43:08 +0100

    fix: escape parameters with restricted keywords

--- test/Riok.Mapperly.Tests/Mapping/MapperTest.cs
+++ test/Riok.Mapperly.Tests/Mapping/MapperTest.cs
@@ -147,4 +147,11 @@ public class MapperTest
 
         return TestHelper.VerifyGenerator(source);
     }
+
+    [Fact]
+    public void RestrictedKeywordParametersShouldBeEscaped()
+    {
+        var source = TestSourceBuilder.MapperWithBody("public partial string Map(int @object);");
+        TestHelper.GenerateMapper(source).Should().HaveSingleMethodBody("return @object.ToString();");
+    }
 }
