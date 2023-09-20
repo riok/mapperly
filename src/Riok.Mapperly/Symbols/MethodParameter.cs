8f7528a8b60fc7ffc3a3378e46ba48d88f54ab9b
Tim M <49349513+TimothyMakkison@users.noreply.github.com>
2023-09-20 07:11:16 +0100

    fix: escape method parameters with reserved keywords (#756)

--- src/Riok.Mapperly/Symbols/MethodParameter.cs
+++ src/Riok.Mapperly/Symbols/MethodParameter.cs
@@ -6,8 +6,14 @@ namespace Riok.Mapperly.Symbols;
 
 public readonly record struct MethodParameter(int Ordinal, string Name, ITypeSymbol Type)
 {
+    private static readonly SymbolDisplayFormat _parameterNameFormat =
+        new(
+            parameterOptions: SymbolDisplayParameterOptions.IncludeName,
+            miscellaneousOptions: SymbolDisplayMiscellaneousOptions.EscapeKeywordIdentifiers
+        );
+
     public MethodParameter(IParameterSymbol symbol)
-        : this(symbol.Ordinal, symbol.Name, symbol.Type.UpgradeNullable()) { }
+        : this(symbol.Ordinal, symbol.ToDisplayString(_parameterNameFormat), symbol.Type.UpgradeNullable()) { }
 
     public MethodArgument WithArgument(ExpressionSyntax? argument) =>
         new(this, argument ?? throw new ArgumentNullException(nameof(argument)));
