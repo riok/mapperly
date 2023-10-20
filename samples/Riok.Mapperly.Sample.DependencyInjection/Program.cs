97eca8d452e456403e09858bd3f36ee5f6c43e57
ZLoo <kozlov.s.v.1992@gmail.com>
2023-10-20 11:17:26 +0300

    Add files via upload
    Add Mediator and Mapperly DI

---
+++ samples/Riok.Mapperly.Sample.DependencyInjection/Program.cs
@@ -0,0 +1,30 @@
+using System.Reflection;
+using Riok.Mapperly.Sample.DependencyInjection.Features.CreateCar.Mapperly;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICreateCarMapperly, CreateCarMapperly>();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
