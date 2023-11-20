86408ce0e21d5cef207c3df003e54313d992373d
Jan Trejbal <jan@trejbal.land>
2023-11-20 18:27:14 +0100

    fix: extension method parameters for existing target mappings (#923)

---
+++ test/Riok.Mapperly.Tests/Mapping/ExtensionMethodTest.cs
@@ -0,0 +1,29 @@
+﻿namespace Riok.Mapperly.Tests.Mapping;
+
+[UsesVerify]
+public class ExtensionMethodTest
+{
+    [Fact]
+    public Task ExtensionMapMethodShouldWork()
+    {
+        var source = TestSourceBuilder.MapperWithBodyAndTypes(
+            "static partial B MapToB(this A source);",
+            "class A { public int Value { get; set; } }",
+            "class B { public int Value { get; set; } }"
+        );
+
+        return TestHelper.VerifyGenerator(source);
+    }
+
+    [Fact]
+    public Task ExtensionUpdateMethodShouldWork()
+    {
+        var source = TestSourceBuilder.MapperWithBodyAndTypes(
+            "static partial void MapToB(this A source, B target);",
+            "class A { public int Value { get; set; } }",
+            "class B { public int Value { get; set; } }"
+        );
+
+        return TestHelper.VerifyGenerator(source);
+    }
+}
