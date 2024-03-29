608c7a3f74ee04bb05800ff9efcd65eef8dbbd38
Timothy Makkison <timmakkison@gmail.com>
2024-03-29 13:54:24 +0000

    chore: fix `ShouldMergeMapperConfigurations`

--- test/Riok.Mapperly.Tests/Helpers/MapperConfigurationBuilderTest.cs
+++ test/Riok.Mapperly.Tests/Helpers/MapperConfigurationBuilderTest.cs
@@ -94,8 +94,8 @@ public class MapperConfigurationBuilderTest
         if (type == typeof(bool))
             return !modifiedValue;
 
-        if (type == typeof(int))
-            return !modifiedValue;
+        if (type == typeof(uint))
+            return (uint)1;
 
         if (type.IsEnum)
             return type.GetEnumValues().GetValue(modifiedValue ? 1 : 0);
