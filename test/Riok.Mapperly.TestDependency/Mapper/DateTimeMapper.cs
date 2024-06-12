e99d0599efab02e5ba323156d037aeccf69217bd
Jan Trejbal <jan@trejbal.land>
2024-06-12 16:35:23 +0200

    Simplify DateTimeMapper

--- test/Riok.Mapperly.TestDependency/Mapper/DateTimeMapper.cs
+++ test/Riok.Mapperly.TestDependency/Mapper/DateTimeMapper.cs
@@ -1,10 +1,8 @@
 using System;
-using Riok.Mapperly.Abstractions;
 
 namespace Riok.Mapperly.TestDependency.Mapper
 {
-    [Mapper]
-    public static partial class DateTimeMapper
+    public static class DateTimeMapper
     {
         public static DateTimeOffset MapToDateTimeOffset(DateTime dateTime) => new(dateTime, TimeSpan.Zero);
     }
