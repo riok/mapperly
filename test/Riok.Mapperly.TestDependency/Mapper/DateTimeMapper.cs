5224b4e8b18f06439a1b0be73aba29cb0b379ac3
Jan Trejbal <jan@trejbal.land>
2024-07-26 11:33:50 +0200

    Drop Riok.Mapperly.TestDependency

--- test/Riok.Mapperly.TestDependency/Mapper/DateTimeMapper.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Riok.Mapperly.TestDependency.Mapper
-{
-    public static class DateTimeMapper
-    {
-        public static DateTimeOffset MapToDateTimeOffset(DateTime dateTime) => new(dateTime, TimeSpan.Zero);
-    }
-}
