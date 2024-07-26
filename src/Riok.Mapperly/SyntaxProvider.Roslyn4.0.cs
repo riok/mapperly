5065644827262d67077d7efc6b67473513e24063
Jan Trejbal <jan@trejbal.land>
2024-07-26 14:26:59 +0200

    Add GetMetadataReferencesProvider

--- src/Riok.Mapperly/SyntaxProvider.Roslyn4.0.cs
+++ src/Riok.Mapperly/SyntaxProvider.Roslyn4.0.cs
@@ -17,11 +17,35 @@ internal static class SyntaxProvider
         IncrementalGeneratorInitializationContext context
     ) =>
         context
-            .MetadataReferencesProvider.SelectMany(
-                (metadataReference, _) => new[] { (metadataReference as CompilationReference)?.Compilation }.ToImmutableArray()
-            )
+            .GetMetadataReferencesProvider()
+            .SelectMany((metadataReference, _) => new[] { (metadataReference as CompilationReference)?.Compilation }.ToImmutableArray())
             .WhereNotNull()
             .Collect();
+
+    /// <summary>
+    /// Workaround to mitigate binary incompatibility introduced in Microsoft.CodeAnalysis=4.2
+    /// <link cref="https://github.com/dotnet/roslyn/issues/61333#issuecomment-1129073030"/>
+    /// </summary>
+    private static IncrementalValuesProvider<MetadataReference> GetMetadataReferencesProvider(
+        this IncrementalGeneratorInitializationContext context
+    )
+    {
+        var metadataProviderProperty =
+            context.GetType().GetProperty(nameof(context.MetadataReferencesProvider))
+            ?? throw new Exception($"The property '{nameof(context.MetadataReferencesProvider)}' not found");
+
+        var metadataProvider = metadataProviderProperty.GetValue(context);
+
+        if (metadataProvider is IncrementalValuesProvider<MetadataReference> metadataValuesProvider)
+            return metadataValuesProvider;
+
+        if (metadataProvider is IncrementalValueProvider<MetadataReference> metadataValueProvider)
+            return metadataValueProvider.SelectMany(static (reference, _) => ImmutableArray.Create(reference));
+
+        throw new Exception(
+            $"The '{nameof(context.MetadataReferencesProvider)}' is neither an '{nameof(IncrementalValuesProvider<MetadataReference>)}<{nameof(MetadataReference)}>' nor an '{nameof(IncrementalValueProvider<MetadataReference>)}<{nameof(MetadataReference)}>.'"
+        );
+    }
 
     public static IncrementalValuesProvider<MapperDeclaration> GetMapperDeclarations(IncrementalGeneratorInitializationContext context)
     {
