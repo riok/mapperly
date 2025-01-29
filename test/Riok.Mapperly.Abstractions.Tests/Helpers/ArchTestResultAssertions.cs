79f28d084d118b579d99f0625a734f1165c2af47
latonz <lars@riok.ch>
2025-01-29 08:22:33 +0100

    fix: build with new fluent assertion version

--- test/Riok.Mapperly.Abstractions.Tests/Helpers/ArchTestResultAssertions.cs
+++ test/Riok.Mapperly.Abstractions.Tests/Helpers/ArchTestResultAssertions.cs
@@ -1,9 +1,11 @@
+using FluentAssertions.Execution;
 using FluentAssertions.Primitives;
 using NetArchTest.Rules;
 
 namespace Riok.Mapperly.Abstractions.Tests.Helpers;
 
-internal class ArchTestResultAssertions(TestResult value) : ObjectAssertions<TestResult, ArchTestResultAssertions>(value)
+internal class ArchTestResultAssertions(TestResult value)
+    : ObjectAssertions<TestResult, ArchTestResultAssertions>(value, AssertionChain.GetOrCreate())
 {
     public void BeSuccessful()
     {
