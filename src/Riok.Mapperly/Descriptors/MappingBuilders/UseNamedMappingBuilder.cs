dcc807f3f8df011b39203ae1acfbc51faf120f58
clegoz <10047369+clegoz@users.noreply.github.com>
2025-03-03 04:13:28 +0300

    refactor: move implementation to UseNamedMappingBuilder

--- src/Riok.Mapperly/Descriptors/MappingBuilders/UseNamedMappingBuilder.cs
+++ src/Riok.Mapperly/Descriptors/MappingBuilders/UseNamedMappingBuilder.cs
@@ -1,5 +1,6 @@
 using Microsoft.CodeAnalysis;
 using Riok.Mapperly.Descriptors.Mappings;
+using Riok.Mapperly.Descriptors.Mappings.ExistingTarget;
 using Riok.Mapperly.Diagnostics;
 
 namespace Riok.Mapperly.Descriptors.MappingBuilders;
@@ -35,6 +36,78 @@ public static class UseNamedMappingBuilder
         }
 
         return differentTargetType ? TryMapTarget(ctx, mapping) : mapping;
+    }
+
+    public static IExistingTargetMapping? TryBuildExistingTargetMapping(MappingBuilderContext ctx)
+    {
+        if (ctx.MappingKey.Configuration.UseNamedMapping == null)
+            return null;
+
+        var useNamedMapping = ctx.MappingKey.Configuration.UseNamedMapping;
+        var existingTargetMapping = ctx.FindExistingTargetNamedMapping(useNamedMapping);
+        if (existingTargetMapping is null)
+            return null;
+
+        var source = ctx.Source;
+        var target = ctx.Target;
+
+        var differentSourceType = !SymbolEqualityComparer.IncludeNullability.Equals(source, existingTargetMapping.SourceType);
+        var differentTargetType = !SymbolEqualityComparer.IncludeNullability.Equals(target, existingTargetMapping.TargetType);
+
+        if (!differentSourceType && !differentTargetType)
+        {
+            return new DelegateExistingTargetMapping(source, target, existingTargetMapping);
+        }
+
+        if (differentSourceType)
+        {
+            ctx.ReportDiagnostic(
+                DiagnosticDescriptors.ReferencedMappingSourceTypeMismatch,
+                useNamedMapping,
+                existingTargetMapping.SourceType.ToDisplayString(),
+                ctx.Source.ToDisplayString()
+            );
+        }
+
+        if (differentTargetType)
+        {
+            ctx.ReportDiagnostic(
+                DiagnosticDescriptors.ReferencedMappingTargetTypeMismatch,
+                useNamedMapping,
+                existingTargetMapping.TargetType.ToDisplayString(),
+                ctx.Target.ToDisplayString()
+            );
+        }
+
+        return TryBuildCompositeExistingTargetMapping(ctx, existingTargetMapping);
+    }
+
+    private static IExistingTargetMapping? TryBuildCompositeExistingTargetMapping(
+        MappingBuilderContext ctx,
+        IExistingTargetMapping existingTargetMapping
+    )
+    {
+        var sourceMapping = ctx.FindOrBuildMapping(ctx.Source, existingTargetMapping.SourceType);
+        if (sourceMapping == null)
+        {
+            ctx.ReportDiagnostic(DiagnosticDescriptors.CouldNotCreateMapping, ctx.Source, existingTargetMapping.SourceType);
+
+            return null;
+        }
+
+        var targetMapping = ctx.FindOrBuildMapping(ctx.Target, existingTargetMapping.TargetType);
+        if (targetMapping == null)
+        {
+            ctx.ReportDiagnostic(
+                DiagnosticDescriptors.CouldNotCreateMapping,
+                ctx.Target.ToDisplayString(),
+                existingTargetMapping.TargetType.ToDisplayString()
+            );
+
+            return null;
+        }
+
+        return new CompositeExistingTargetMapping(existingTargetMapping, sourceMapping, targetMapping);
     }
 
     private static INewInstanceMapping? TryMapTarget(MappingBuilderContext ctx, INewInstanceMapping mapping)
