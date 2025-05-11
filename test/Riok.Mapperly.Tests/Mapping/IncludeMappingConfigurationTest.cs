438161970eb3cc3fb2e4980a7390eb02eccf946c
faddiv <faddiv@gmail.com>
2025-05-11 10:23:07 +0200

    Add additional tests for IncludeMappingConfiguration

--- test/Riok.Mapperly.Tests/Mapping/IncludeMappingConfigurationTest.cs
+++ test/Riok.Mapperly.Tests/Mapping/IncludeMappingConfigurationTest.cs
@@ -3,11 +3,87 @@
 public class IncludeMappingConfigurationTest
 {
     [Fact]
-    public Task IncludeMappingConfigurationUsesMapPropertyFromTargetMapper()
+    public Task UsesMapPropertyFromTargetMapper()
     {
         var source = TestSourceBuilder.MapperWithBodyAndTypes(
             """
             [MapProperty(nameof(A.SourceName), nameof(B.DestinationName))]
+            partial B OtherMappingMethod(A a);
+
+            [IncludeMappingConfiguration(nameof(OtherMappingMethod))]
+            partial B MapAnother(A a);
+            """,
+            "class A { public string SourceName { get; set; } }",
+            "class B { public string DestinationName { get; set; } }"
+        );
+
+        return TestHelper.VerifyGenerator(source);
+    }
+
+    [Fact]
+    public Task UsesMapperIgnoreSourceFromTargetMapper()
+    {
+        var source = TestSourceBuilder.MapperWithBodyAndTypes(
+            """
+            [MapperIgnoreSource(nameof(A.Ignored))]
+            partial B OtherMappingMethod(A a);
+
+            [IncludeMappingConfiguration(nameof(OtherMappingMethod))]
+            partial B MapAnother(A a);
+            """,
+            "class A { public string Property { get; set; } public string Ignored { get; set; } }",
+            "class B { public string Property { get; set; } }"
+        );
+
+        return TestHelper.VerifyGenerator(source);
+    }
+
+    [Fact]
+    public Task UsesMapperIgnoreTargetFromTargetMapper()
+    {
+        var source = TestSourceBuilder.MapperWithBodyAndTypes(
+            """
+            [MapperIgnoreTarget(nameof(A.Ignored))]
+            partial B OtherMappingMethod(A a);
+
+            [IncludeMappingConfiguration(nameof(OtherMappingMethod))]
+            partial B MapAnother(A a);
+            """,
+            "class A { public string Property { get; set; } }",
+            "class B { public string Property { get; set; } public string Ignored { get; set; } }"
+        );
+
+        return TestHelper.VerifyGenerator(source);
+    }
+
+    [Fact]
+    public Task AppliesRecursively()
+    {
+        var source = TestSourceBuilder.MapperWithBodyAndTypes(
+            """
+            [MapProperty(nameof(A.SourceName), nameof(B.DestinationName))]
+            partial B BaseMappingMethod(A a);
+
+            [IncludeMappingConfiguration(nameof(BaseMappingMethod))]
+            partial B FirstMappingMethod(A a);
+
+            [IncludeMappingConfiguration(nameof(FirstMappingMethod))]
+            partial B SecondMappingMethod(A a);
+            """,
+            "class A { public string SourceName { get; set; } }",
+            "class B { public string DestinationName { get; set; } }"
+        );
+
+        return TestHelper.VerifyGenerator(source);
+    }
+
+    [Fact]
+    public Task DetectsCircularReferences()
+    {
+        var source = TestSourceBuilder.MapperWithBodyAndTypes(
+            """
+            [MapProperty(nameof(A.SourceName), nameof(B.DestinationName))]
+            [IncludeMappingConfiguration(nameof(MapAnother))]
             partial B OtherMappingMethod(A a);
 
             [IncludeMappingConfiguration(nameof(OtherMappingMethod))]
