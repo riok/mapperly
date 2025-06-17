09e8a4b9eba4d1547536782c9a77f5682cb6811f
faddiv <faddiv@gmail.com>
2025-06-17 10:13:24 +0200

    Add NamedMappingAttribute

---
+++ src/Riok.Mapperly.Abstractions/NamedMappingAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace Riok.Mapperly.Abstractions;
+
+/// <summary>
+/// Specifies a custom name for the mapping. This can be used to reference in other mapping configurations.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+[Conditional("MAPPERLY_ABSTRACTIONS_SCOPE_RUNTIME")]
+public sealed class NamedMappingAttribute(string name) : Attribute
+{
+    /// <summary>
+    /// Gets the custom name specified for the mapping.
+    /// </summary>
+    public string Name { get; } = name;
+}
