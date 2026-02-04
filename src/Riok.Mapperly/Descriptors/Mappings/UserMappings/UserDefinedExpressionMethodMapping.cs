d514cbc99f8bfeeb437219d3e3ff7d564529aa0e
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2026-02-05 01:27:17 +0200

    simplyify

--- src/Riok.Mapperly/Descriptors/Mappings/UserMappings/UserDefinedExpressionMethodMapping.cs
+++ src/Riok.Mapperly/Descriptors/Mappings/UserMappings/UserDefinedExpressionMethodMapping.cs
@@ -3,8 +3,7 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Riok.Mapperly.Emit;
 using Riok.Mapperly.Emit.Syntax;
-using Riok.Mapperly.Helpers;
-using Riok.Mapperly.Symbols;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace Riok.Mapperly.Descriptors.Mappings.UserMappings;
 
@@ -16,7 +15,6 @@ public class UserDefinedExpressionMethodMapping : NewInstanceMethodMapping, INew
 {
     private INewInstanceMapping? _delegateMapping;
     private readonly IMethodSymbol _method;
-    private readonly MethodDeclarationSyntax? _methodDeclarationSyntax;
 
     public UserDefinedExpressionMethodMapping(
         IMethodSymbol method,
@@ -27,10 +25,8 @@ public class UserDefinedExpressionMethodMapping : NewInstanceMethodMapping, INew
         : base(expressionSourceType, returnType)
     {
         _method = method;
-        _methodDeclarationSyntax = method.DeclaringSyntaxReferences.FirstOrDefault()?.GetSyntax() as MethodDeclarationSyntax;
         ExpressionSourceType = expressionSourceType;
         ExpressionTargetType = expressionTargetType;
-        // Force the method name to be preserved
         SetMethodNameIfNeeded(_ => method.Name);
     }
 
@@ -46,72 +42,46 @@ public class UserDefinedExpressionMethodMapping : NewInstanceMethodMapping, INew
     /// </summary>
     public ITypeSymbol ExpressionTargetType { get; }
 
-    public bool? Default => false; // Expression mappings are never default mappings
+    public bool? Default => false;
 
     public bool IsExternal => false;
 
     public void SetDelegateMapping(INewInstanceMapping mapping) => _delegateMapping = mapping;
 
-    public override ExpressionSyntax Build(TypeMappingBuildContext ctx)
-    {
-        // Expression mappings generate code directly and don't call other methods
+    public override ExpressionSyntax Build(TypeMappingBuildContext ctx) =>
         throw new InvalidOperationException("Expression mappings should not be built as expressions.");
-    }
 
     public override IEnumerable<StatementSyntax> BuildBody(TypeMappingBuildContext ctx)
     {
         if (_delegateMapping == null)
-        {
             return [ctx.SyntaxFactory.ExpressionStatement(ctx.SyntaxFactory.ThrowMappingNotImplementedException())];
-        }
 
-        // The delegate mapping is an ExpressionMapping that builds the actual expression lambda
         return _delegateMapping is MethodMapping mm ? mm.BuildBody(ctx) : [ctx.SyntaxFactory.Return(_delegateMapping.Build(ctx))];
     }
 
     public override MethodDeclarationSyntax BuildMethod(SourceEmitterContext ctx)
     {
-        var typeMappingBuildContext = new TypeMappingBuildContext(
-            "source", // dummy source name, not used
-            null,
-            ctx.NameBuilder.NewScope(),
-            ctx.SyntaxFactory
-        );
+        var typeMappingBuildContext = new TypeMappingBuildContext("source", null, ctx.NameBuilder.NewScope(), ctx.SyntaxFactory);
 
-        var parameters = BuildParameterList();
-        var returnType = SyntaxFactoryHelper.FullyQualifiedIdentifier(TargetType);
-        return Microsoft
-            .CodeAnalysis.CSharp.SyntaxFactory.MethodDeclaration(
-                returnType.AddTrailingSpace(),
-                Microsoft.CodeAnalysis.CSharp.SyntaxFactory.Identifier(_method.Name)
-            )
-            .WithModifiers(Microsoft.CodeAnalysis.CSharp.SyntaxFactory.TokenList(BuildModifiers(ctx.IsStatic)))
-            .WithParameterList(parameters)
+        return MethodDeclaration(SyntaxFactoryHelper.FullyQualifiedIdentifier(TargetType).AddTrailingSpace(), Identifier(_method.Name))
+            .WithModifiers(TokenList(BuildModifiers()))
+            .WithParameterList(ParameterList())
             .WithAttributeLists(BuildAttributes(typeMappingBuildContext))
             .WithBody(ctx.SyntaxFactory.Block(BuildBody(typeMappingBuildContext.AddIndentation())));
     }
 
-    protected override ParameterListSyntax BuildParameterList()
-    {
-        // No parameters for expression-returning methods
-        return Microsoft.CodeAnalysis.CSharp.SyntaxFactory.ParameterList();
-    }
+    protected override ParameterListSyntax BuildParameterList() => ParameterList();
 
-    private IEnumerable<SyntaxToken> BuildModifiers(bool isStatic)
+    private SyntaxTokenList BuildModifiers()
     {
-        // Copy modifiers from the original method declaration
-        if (_methodDeclarationSyntax != null)
-        {
-            return _methodDeclarationSyntax.Modifiers.Select(x => SyntaxFactoryHelper.TrailingSpacedToken(x.Kind()));
-        }
+        if (_method.DeclaringSyntaxReferences.FirstOrDefault()?.GetSyntax() is MethodDeclarationSyntax methodSyntax)
+            return methodSyntax.Modifiers;
 
-        // Fallback to private (and optionally static)
-        return isStatic
-            ?
-            [
-                SyntaxFactoryHelper.TrailingSpacedToken(Microsoft.CodeAnalysis.CSharp.SyntaxKind.PrivateKeyword),
-                SyntaxFactoryHelper.TrailingSpacedToken(Microsoft.CodeAnalysis.CSharp.SyntaxKind.StaticKeyword),
-            ]
-            : [SyntaxFactoryHelper.TrailingSpacedToken(Microsoft.CodeAnalysis.CSharp.SyntaxKind.PrivateKeyword)];
+        // Fallback - shouldn't happen for partial methods
+        return TokenList(
+            SyntaxFactoryHelper.TrailingSpacedToken(SyntaxKind.PrivateKeyword),
+            SyntaxFactoryHelper.TrailingSpacedToken(SyntaxKind.StaticKeyword),
+            SyntaxFactoryHelper.TrailingSpacedToken(SyntaxKind.PartialKeyword)
+        );
     }
 }
