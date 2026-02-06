bb9bd40bf7dc13be79fa4a685b25305fe4c9fb7b
Alessandro Losi <a.losi@biware.it>
2026-02-06 17:27:55 +0100

    Added tests for both cases of shallow cloning with and without [UserMapping(Default = false)]

--- test/Riok.Mapperly.Tests/Mapping/ShallowCloneTest.cs
+++ test/Riok.Mapperly.Tests/Mapping/ShallowCloneTest.cs
@@ -1,4 +1,6 @@
-﻿namespace Riok.Mapperly.Tests.Mapping;
+﻿using Riok.Mapperly.Abstractions;
+
+namespace Riok.Mapperly.Tests.Mapping;
 
 public class ShallowCloneTest
 {
@@ -9,7 +11,14 @@ public class ShallowCloneTest
             "A",
             "A",
             TestSourceBuilderOptions.WithShallowCloning,
-            "class A { public int Value { get; set; } public List<string> List { get; set; } }"
+            """
+            class A
+            {
+                public int Value { get; set; }
+                public List<string> List { get; set; }
+                public A NestedReference { get; set; }
+            }"
+            """
         );
         TestHelper
             .GenerateMapper(source)
@@ -19,6 +28,44 @@ public class ShallowCloneTest
                 var target = new global::A();
                 target.Value = source.Value;
                 target.List = source.List;
+                target.NestedReference = Map(source.NestedReference);
+                return target;
+                """
+            );
+    }
+
+    [Fact]
+    public void ShallowCloneWithNoUserMappingsShouldDirectAssignAllProperties()
+    {
+        var source = TestSourceBuilder.CSharp(
+            """
+            using Riok.Mapperly.Abstractions;
+
+            [Mapper(CloningStrategy = CloningStrategy.ShallowCloning)]
+            public partial class Mapper
+            {
+                [UserMapping(Default = false)]
+                public partial A Map(A source);
+            }
+
+            class A
+            {
+                public int Value { get; set; }
+                public List<string> List { get; set; }
+                public A NestedReference { get; set; }
+            }
+            """
+        );
+
+        TestHelper
+            .GenerateMapper(source)
+            .Should()
+            .HaveSingleMethodBody(
+                """
+                var target = new global::A();
+                target.Value = source.Value;
+                target.List = source.List;
+                target.NestedReference = source.NestedReference;
                 return target;
                 """
             );
