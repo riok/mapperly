066fd2dfacb429ad0be90d13054b479649b3fceb
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2026-02-07 14:49:52 +0200

    Cleanup UserDefinedExpressionMethodMapping

--- src/Riok.Mapperly/Descriptors/Mappings/UserMappings/UserDefinedExpressionMethodMapping.cs
+++ src/Riok.Mapperly/Descriptors/Mappings/UserMappings/UserDefinedExpressionMethodMapping.cs
@@ -10,40 +10,26 @@ namespace Riok.Mapperly.Descriptors.Mappings.UserMappings;
 /// Unlike IQueryable mappings which have a source parameter and can use <see cref="UserDefinedNewInstanceMethodMapping"/>,
 /// Expression mappings are parameterless, so the source/target types must be extracted from the return type's type arguments.
 /// </summary>
-public class UserDefinedExpressionMethodMapping : NewInstanceMethodMapping, INewInstanceUserMapping
+public class UserDefinedExpressionMethodMapping(
+    IMethodSymbol method,
+    ITypeSymbol expressionSourceType,
+    ITypeSymbol expressionTargetType,
+    ITypeSymbol returnType
+) : NewInstanceMethodMapping(method, CreateSourceParameter(expressionSourceType), null, returnType), INewInstanceUserMapping
 {
-    /// <summary>
-    /// Dummy source parameter required by the base <see cref="MethodMapping"/> constructor.
-    /// The actual lambda parameter name is generated fresh by <see cref="TypeMappingBuildContext.WithNewScopedSource"/>,
-    /// so this value is never used in the generated code. We override <see cref="BuildParameterList"/> to emit no parameters.
-    /// </summary>
-    private static readonly MethodParameter _dummySourceParameter = new(0, "_", null!);
-
     private INewInstanceMapping? _delegateMapping;
 
-    public UserDefinedExpressionMethodMapping(
-        IMethodSymbol method,
-        ITypeSymbol expressionSourceType,
-        ITypeSymbol expressionTargetType,
-        ITypeSymbol returnType
-    )
-        : base(method, _dummySourceParameter, null, returnType)
-    {
-        ExpressionSourceType = expressionSourceType;
-        ExpressionTargetType = expressionTargetType;
-    }
-
-    public new IMethodSymbol Method => base.Method!;
+    public new IMethodSymbol Method { get; } = method;
 
     /// <summary>
     /// The source type of the expression (TSource in Expression{Func{TSource, TTarget}}).
     /// </summary>
-    public ITypeSymbol ExpressionSourceType { get; }
+    public ITypeSymbol ExpressionSourceType { get; } = expressionSourceType;
 
     /// <summary>
     /// The target type of the expression (TTarget in Expression{Func{TSource, TTarget}}).
     /// </summary>
-    public ITypeSymbol ExpressionTargetType { get; }
+    public ITypeSymbol ExpressionTargetType { get; } = expressionTargetType;
 
     public bool? Default => false;
 
@@ -60,4 +46,11 @@ public class UserDefinedExpressionMethodMapping : NewInstanceMethodMapping, INew
     }
 
     protected override ParameterListSyntax BuildParameterList() => ParameterList();
+
+    /// <summary>
+    /// Dummy source parameter required by the base <see cref="MethodMapping"/> constructor.
+    /// We override <see cref="BuildParameterList"/> to emit no parameters, so this value
+    /// is never used in the generated code.
+    /// </summary>
+    private static MethodParameter CreateSourceParameter(ITypeSymbol expressionSourceType) => new(0, "_", expressionSourceType);
 }
