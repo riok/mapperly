ac8d93f906e051408f48aef039e8de2e42f0bf27
Federico Dipuma <federico.dipuma@docplanner.com>
2026-07-13 22:40:51 +0200

    test: cover ignored projection of nullable navigation to non-nullable dto (#2350)

--- test/Riok.Mapperly.Tests/Mapping/QueryableProjectionNullHandlingTest.cs
+++ test/Riok.Mapperly.Tests/Mapping/QueryableProjectionNullHandlingTest.cs
@@ -40,4 +40,24 @@ public class QueryableProjectionNullHandlingTest
                 """
             );
     }
+
+    [Fact]
+    public Task IgnoreShouldNotRequireParameterlessCtorForNullableNavigation()
+    {
+        // https://github.com/riok/mapperly/issues/2350
+        var source = TestSourceBuilder.Mapping(
+            "System.Linq.IQueryable<A>",
+            "System.Linq.IQueryable<B>",
+            TestSourceBuilderOptions.Default with
+            {
+                QueryableProjectionNullHandling = QueryableProjectionNullHandling.Ignore,
+            },
+            "class A { public C? Nested { get; set; } }",
+            "class B { public D Nested { get; set; } }",
+            "class C { public int Id { get; set; } }",
+            "class D { public D(int id) { Id = id; } public int Id { get; } }"
+        );
+
+        return TestHelper.VerifyGenerator(source);
+    }
 }
